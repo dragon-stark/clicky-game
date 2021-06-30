b1d6acf4380f2a8ba19b9e054d43f7a16ec500e7
Elizabeth <Hindash@email.arizona.edu>
2021-06-30 13:27:41 -0700

    React error fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,7 @@ import "./style.css";
 function Header() {
   return (
     <header className="header">
-      <h1>CLICKY <br>
-      </br> The Office memory game!</h1>
+      <h1>CLICKY The Office memory game!</h1>
       <h2>
        This game is the best way to test your memory! <br>
       To begin take a look at the images in the squares.   </br>Then simply click on an image, continue to click on the images until you have clicked every character.  If you click on the same character more than once during a game you will get an alert and you will need to restart your round.  
