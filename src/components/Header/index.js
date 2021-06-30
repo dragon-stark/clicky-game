20ca9bdcc36377a5b4ac543dc486382d262eb5fd
Elizabeth <Hindash@email.arizona.edu>
2021-06-30 13:31:38 -0700

    further back

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,8 +6,7 @@ function Header() {
     <header className="header">
       <h1>Clicky, The Office memory game!</h1>
       <h2>
-       This game is the best way to test your memory! To begin take a look at the images in the squares.  Then simply click on an image, continue to click on the images until you have clicked every character.  If you click on the same character more than once during a game you will get an alert and you will need to restart your round.  
-       Your points will be displayed at the top of this screen.  
+       This game is the best way to test your memory! To begin take a look at the images in the squares.  Then simply click on an image, continue to click on the images until you have clicked every character.  If you click on the same character more than once during a game you will get an alert and you will need to restart your round.  Your points will be displayed at the top of this screen.  
       </h2>
     </header>
   );
