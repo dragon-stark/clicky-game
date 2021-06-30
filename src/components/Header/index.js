d4001ba2a4d6f8e391cd9702cff9c9de1f816c81
Elizabeth <Hindash@email.arizona.edu>
2021-06-30 13:28:58 -0700

    backtracking this error

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,11 +4,11 @@ import "./style.css";
 function Header() {
   return (
     <header className="header">
-      <h1>CLICKY The Office memory game!</h1>
+      <h1>Clicky, The Office memory game!</h1>
       <h2>
        This game is the best way to test your memory! <br>
       To begin take a look at the images in the squares.   </br>Then simply click on an image, continue to click on the images until you have clicked every character.  If you click on the same character more than once during a game you will get an alert and you will need to restart your round.  
-       <br>Your points will be displayed at the top of this screen.  </br>
+       Your points will be displayed at the top of this screen.  
       </h2>
     </header>
   );
